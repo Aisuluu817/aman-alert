a2e3bea1f777a3d6a5ffc11549c4763bbe121b0f
Aisuluu <a.arstanbaeva01@gmail.com>
2023-04-15 22:15:02 +0600

    Initial Commit

---
+++ AmanAlert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmanAlert
+//
+//  Created by Arstanbaeva Aisuluu on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

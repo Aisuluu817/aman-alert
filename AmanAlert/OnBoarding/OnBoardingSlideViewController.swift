92b38e14412bb300f7733f7a615137d744ef2e05
Aisuluu817 <a.arstanbaeva01@gmail.com>
2023-04-22 22:12:02 +0600

    PageViewController for all Onboarding Screens

---
+++ AmanAlert/OnBoarding/OnBoardingSlideViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnBoardingSlide.swift
+//  AmanAlert
+//
+//  Created by Arstanbaeva Aisuluu on 22.04.2023.
+//
+
+import Foundation
+import UIKit
+
+struct OnBoardingSlide {
+    let title: String
+    let image: UIImage
+    let description: String
+}

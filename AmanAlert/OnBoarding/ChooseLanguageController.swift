50a874c454e9e07634a4f64ca1dea17fb7b883ac
Aisuluu817 <a.arstanbaeva01@gmail.com>
2023-04-25 21:31:51 +0600

    choose language controller added

--- AmanAlert/OnBoarding/ChooseLanguageController.swift
+++ AmanAlert/OnBoarding/ChooseLanguageController.swift
@@ -1,8 +1,32 @@
 import Foundation
 import UIKit
+import SnapKit
 
 
 class ChooseLanguageController: UIViewController {
+    
+    let image = UIImageView(image: UIImage(named: "ic_aman_blue"))
+    
+    let label: UILabel = {
+        let text = UILabel()
+        text.text = "Выберите язык для \nиспользования"
+        text.font = .boldSystemFont(ofSize: 28)
+        text.textAlignment = .center
+        text.textColor = .black
+        text.numberOfLines = 2
+        return text
+    }()
+    
+    lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 22
+        button.backgroundColor = UIColor(named: "blueButtonColor")
+        button.setTitle("Выбрать", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        return button
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,15 +36,33 @@ class ChooseLanguageController: UIViewController {
     }
     
     func setUpView() {
-        let stackView = UIStackView()
-        stackView.distribution = .fillEqually
+        view.backgroundColor = .white
+        title = "Main"
     }
     
     func configure() {
-        
+        view.addSubview(image)
+        view.addSubview(label)
+        view.addSubview(startButton)
     }
     
     func setUpConstraints() {
+        image.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(120)
+        }
         
+        label.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(image.snp.bottom).offset(10)
+        }
+        
+        startButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-40)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(290)
+            $0.height.equalTo(45)
+        }
     }
 }
+

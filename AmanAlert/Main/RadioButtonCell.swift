2ba3327d0419c54b7cbfdaedf3f1e6ae6c87d7e1
Aisuluu817 <a.arstanbaeva01@gmail.com>
2023-04-26 22:39:53 +0600

    Custom table view cell

--- AmanAlert/Main/RadioButtonCell.swift
+++ AmanAlert/Main/RadioButtonCell.swift
@@ -3,25 +3,65 @@
 import Foundation
 import UIKit
 
+
+public struct RadioButtonCellModel {
+    public let title: String
+    public let subtitle: String?
+    public var isSelected: Bool
+    
+    public init(title: String,
+                subtitle: String? = nil,
+                isSelected: Bool = false) {
+        self.title = title
+        self.subtitle = subtitle
+        self.isSelected = isSelected
+    }
+}
+
+
 class RadioButtonCell: UITableViewCell {
     
     let radioButton = UIButton()
+    let label = UILabel()
         
         override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
             super.init(style: style, reuseIdentifier: reuseIdentifier)
             setupRadioButton()
+            setupLabel()
+            setUpConstraints()
         }
         
         required init?(coder aDecoder: NSCoder) {
             super.init(coder: aDecoder)
             setupRadioButton()
+            setupLabel()
+            setUpConstraints()
         }
         
+     
         func setupRadioButton() {
-            radioButton.frame = CGRect(x: 10, y: 10, width: 20, height: 20)
+          //  radioButton.frame = CGRect(x: 10, y: 10, width: 20, height: 20)
             radioButton.layer.cornerRadius = 10
-            radioButton.layer.borderWidth = 1
-            radioButton.layer.borderColor = UIColor.gray.cgColor
+            radioButton.setImage(UIImage(named: "ic_radiobutton_unchecked"), for: .normal)
             contentView.addSubview(radioButton)
         }
+    
+        func setupLabel() {
+           // label.frame = CGRect(x: 40, y: 10, width: contentView.bounds.width - 50, height: 20)
+            label.text = "Heleo"
+            label.textColor = .blue
+            contentView.addSubview(label)
+        }
+    
+    func setUpConstraints() {
+        radioButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(8)
+            $0.top.equalToSuperview().offset(5)
+        }
+        
+        label.snp.makeConstraints {
+            $0.leading.equalTo(radioButton.snp.trailing).offset(8)
+            $0.centerY.equalToSuperview()
+        }
+    }
 }
--- AmanAlert/OnBoarding/ChooseLanguageController.swift
+++ AmanAlert/OnBoarding/ChooseLanguageController.swift
@@ -3,9 +3,22 @@ import UIKit
 import SnapKit
 
 
-class ChooseLanguageController: UIViewController {
+class ChooseLanguageController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let tableView = UITableView()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RadioCell", for: indexPath) as! RadioButtonCell
+              cell.backgroundColor = .red
+              cell.label.text = "Option \(indexPath.row + 1)"
+              cell.radioButton.isSelected = indexPath.row == selectedIndex
+              return cell
+    }
+    
+    
+    let tV = UITableView()
         var selectedIndex = -1
     
     let image = UIImageView(image: UIImage(named: "ic_aman_blue"))
@@ -48,7 +61,14 @@ class ChooseLanguageController: UIViewController {
         view.addSubview(image)
         view.addSubview(label)
         view.addSubview(startButton)
-        view.addSubview(tableView)
+        view.addSubview(tV)
+    }
+    
+    func setupTableView() {
+        tV.backgroundColor = .blue
+        tV.register(RadioButtonCell.self, forCellReuseIdentifier: "RadioCell")
+        tV.delegate = self
+        tV.dataSource = self
     }
     
     func setUpConstraints() {
@@ -62,44 +82,38 @@ class ChooseLanguageController: UIViewController {
             $0.top.equalTo(image.snp.bottom).offset(10)
         }
         
-        tableView.snp.makeConstraints {
-            $0.top.equalTo(label.snp.bottom).offset(20)
-            $0.bottom.equalTo(startButton.snp.top).offset(-20)
-        }
-        
         startButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().offset(-40)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(290)
             $0.height.equalTo(45)
         }
+        
+        tV.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(20)
+            $0.bottom.equalTo(startButton.snp.top).offset(-20)
+        }
     }
 }
 
-extension ChooseLanguageController: UITableViewDelegate, UITableViewDataSource {
-    
-    func setupTableView() {
-        tableView.frame = view.bounds
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(RadioButtonCell.self, forCellReuseIdentifier: "RadioButtonCell")
-      
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RadioButtonCell", for: indexPath) as! RadioButtonCell
-        cell.selectionStyle = .none
-        cell.radioButton.isSelected = indexPath.row == selectedIndex
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedIndex = indexPath.row
-        tableView.reloadData()
-    }
-}
+//extension ChooseLanguageController: UITableViewDelegate, UITableViewDataSource {
+//
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 3
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "RadioButtonCell", for: indexPath) as! RadioButtonCell
+//        cell.backgroundColor = .red
+//        cell.label.text = "Option \(indexPath.row + 1)"
+//        cell.radioButton.isSelected = indexPath.row == selectedIndex
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        selectedIndex = indexPath.row
+//        tableView.reloadData()
+//    }
+//}
 

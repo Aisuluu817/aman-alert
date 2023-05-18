2ac8743485e6a509e042673e61567ccea00f3f64
Aisuluu817 <a.arstanbaeva01@gmail.com>
2023-05-19 00:30:47 +0600

    fixed rxMoya SPM

--- AmanAlert/Network/MainTarget.swift
+++ AmanAlert/Network/MainTarget.swift
@@ -1,11 +1,6 @@
-//
-//  MainTarget.swift
-//  AmanAlert
-//
-//  Created by Arstanbaeva Aisuluu on 27.04.2023.
-//
 
 import Foundation
+import RxMoya
 import Moya
 
 enum MainTarget {
@@ -17,7 +12,7 @@ enum MainTarget {
 }
 
 extension MainTarget : TargetType {
-    var task: Moya.Task {
+    var task: Task {
         .requestPlain
     }
     
--- AmanAlert/Network/NetworkManager.swift
+++ AmanAlert/Network/NetworkManager.swift
@@ -1,22 +1,22 @@
-//import Foundation
-//import UIKit
-//import Moya
-//import RxSwift
-//import RxMoya
-//
-//struct NetworkManager {
-//    static let shared = NetworkManager()
-//    
-//    let provider = MoyaProvider<MainTarget>()
-//    
-//    public func getNewsById(with id: Int) -> Single<News> {
-//        provider.rx.request(.getNewsById(id: id))
-//            .map(News.self)
-//    }
-//
-//    public func getAllNews() -> Observable<[News]> {
-//        return provider.rx.
-//            .request(.getNews)
-//            .map([News].self).asObservable()
-//    }
-//}
+import Foundation
+import UIKit
+import Moya
+import RxSwift
+import RxMoya
+
+struct NetworkManager {
+    static let shared = NetworkManager()
+    
+    let provider = MoyaProvider<MainTarget>()
+    
+    public func getNewsById(with id: Int) -> Single<News> {
+        provider.rx.request(.getNewsById(id: id))
+            .map(News.self)
+    }
+
+    public func getAllNews() -> Observable<[News]> {
+        provider.rx.request(.getNews)
+            .map([News].self).asObservable()
+    }
+
+}
